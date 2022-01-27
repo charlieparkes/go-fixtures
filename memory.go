71f9bbe806fad3cbe3055c8a9744f57731f7ec88
Charlie Mathews <charlie@charliemathews.com>
2022-01-27 12:02:57 -0600

    Use tklauser/go-sysconf instead of cgo

--- memory.go
+++ memory.go
@@ -1,14 +1,17 @@
 package fixtures
 
-// #include <unistd.h>
-import "C"
+import (
+	"github.com/tklauser/go-sysconf"
+)
 
-var mem_bytes int64
-
-func init() {
-	mem_bytes = int64(C.sysconf(C._SC_PHYS_PAGES) * C.sysconf(C._SC_PAGE_SIZE))
+func mustSysconf(name int) int64 {
+	x, err := sysconf.Sysconf(name)
+	if err != nil {
+		panic(err)
+	}
+	return x
 }
 
 func memoryMB() int64 {
-	return mem_bytes / 1e6
+	return int64(mustSysconf(sysconf.SC_PHYS_PAGES)*mustSysconf(sysconf.SC_PAGE_SIZE)) / 1e6
 }
